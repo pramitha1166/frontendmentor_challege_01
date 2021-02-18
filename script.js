0fcb89d2b9437d1d90776a014e067050835de4cc
pramitha1166 <63492955+pramitha1166@users.noreply.github.com>
2021-02-19 02:08:54 +0530

    end of JS functions

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var progressbar = document.getElementById("progressbar");
 var price_element = document.getElementById("price_value");
 var views_element = document.getElementById("views");
 
+var isChecked = document.getElementById("checkbox");
+
 //var price = 0;
 var views = 0;
 
@@ -16,23 +18,53 @@ slider.oninput = function() {
 
 
     if(this.value<=5) {
-        price_element.innerHTML = "$8"
+
+        if(isChecked.checked) {
+            price_element.innerHTML = 8*80/100;
+        }else {
+            price_element.innerHTML =  8;
+        }
+        
         views_element.innerHTML = "10K"
     }
     else if (this.value>5 && this.value<=10) {
-        price_element.innerHTML = "$12"
+        
+        if(isChecked.checked) {
+            price_element.innerHTML =  12*80/100;
+        }else {
+            price_element.innerHTML =  12;
+        }
         views_element.innerHTML = "50K"
     }
     else if (this.value>10 && this.value<=50) {
-        price_element.innerHTML = "$16"
+
+        if(isChecked.checked) {
+            price_element.innerHTML = 16*80/100;
+        }else {
+            price_element.innerHTML =  16;
+        }
         views_element.innerHTML = "100K"
     }
     else if(this.value>50 && this.value<100) {
-        price_element.innerHTML = "$24"
+
+        if(isChecked.checked) {
+            price_element.innerHTML = 24*80/100;
+        }else {
+            price_element.innerHTML = 24;
+        }
         views_element.innerHTML = "500K"
     }
     else if(this.value==100) {
-        price_element.innerHTML = "$36"
+        if(isChecked.checked) {
+            price_element.innerHTML = 36*80/100;
+        }else {
+            price_element.innerHTML = 36;
+        }
         views_element.innerHTML = "1M"
     }
 }
+
+function onCheckHandler() {
+    slider.oninput()
+}
+
